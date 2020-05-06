fa7eb6c59ee9b34c6f85c46a1d123b36a03d64ae
zacherysims <zach.md.sims97@gmail.com>
2020-05-06 00:03:14 -0600

    There is now an un-scannable magnet that is too strong lmao

---
+++ Assets/MagnetPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetPoint : MonoBehaviour
+{
+    public float forceFactor = 200f;
+
+    List<Rigidbody> magneticItems = new List<Rigidbody>();
+
+    Transform magnetPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        magnetPoint = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        foreach (Rigidbody magneticItem in magneticItems)
+        {
+            magneticItem.AddForce((magnetPoint.position - magneticItem.position) * forceFactor * Time.fixedDeltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Magnetic"))
+            magneticItems.Add(other.GetComponent<Rigidbody>());
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Magnetic"))
+            magneticItems.Remove(other.GetComponent<Rigidbody>());
+    }
+}
