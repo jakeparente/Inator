bdbd096cc7ef1a1d74977d12382f6badfc7bb6f6
Jake Parente <jakeparente@gmail.com>
2020-05-06 20:06:43 -0600

    Set up puzzle scene

--- Assets/Scripts/Puzzles/StickyPuzzle.cs
+++ Assets/Scripts/Puzzles/StickyPuzzle.cs
@@ -2,24 +2,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StickyPuzzle : MaterialGiver
 {
     public GameObject tophat;
     public GameObject banana;
 
+    public TextMeshProUGUI fancyText;
+    public TextMeshProUGUI bananaText;
+    public TextMeshProUGUI yellowText;
+
     public override void OnShot(GameObject obj)
     {
         if (inator.type == Inator.InatorType.Object && obj != null)
         {
             if (obj.name.Split(char.Parse("("))[0] == "TopHat")
+            {
                 tophat.SetActive(true);
+                fancyText.gameObject.SetActive(false);
+                bananaText.gameObject.SetActive(true);
+            }
             else if (obj.name.Split(char.Parse("("))[0] == "Banana")
+            {
                 banana.SetActive(true);
+                fancyText.gameObject.SetActive(false);
+                bananaText.gameObject.SetActive(false);
+                yellowText.gameObject.SetActive(true);
+            }
             else
-                SpawnObject(obj, false);
+                OnShot(obj);
         }
-        else if (inator.type == Inator.InatorType.Effect && obj != null)
-            SpawnObject(obj, true);
+        else
+            OnShot(obj);
     }
 }
