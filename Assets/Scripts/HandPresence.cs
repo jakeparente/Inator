a500aef9b04837ac857f12f3e1dd52d5796d74ec
Jake Parente <jakeparente@gmail.com>
2020-05-07 01:48:17 -0600

    hands now close but with bugs

---
+++ Assets/Scripts/HandPresence.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    private void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+            targetDevice = devices[0];
+
+        spawnedHandModel = Instantiate(handModelPrefab, transform);
+        handAnimator = spawnedHandModel.GetComponent<Animator>();
+    }
+
+    private void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+            handAnimator.SetFloat("Grip", gripValue);
+        else
+            handAnimator.SetFloat("Grip", 0);
+    }
+
+    void Update()
+    {
+        if (!targetDevice.isValid)
+            TryInitialize();
+        else
+            UpdateHandAnimation();
+    }
+}
