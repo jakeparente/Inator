374f889c55d29cdf2f320dd8212395de66a674ee
Jake Parente <jakeparente@gmail.com>
2020-05-07 18:02:34 -0600

    moved some stuff around

---
+++ Assets/Scripts/Puzzles/TreasureChestLock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChestLock : ScannableObject
+{
+    public override void Start()
+    {
+    }
+
+    public override void OnShot(GameObject obj)
+    {
+        return;
+    }
+
+    public override void OnShot(Material material)
+    {
+        return;
+    }
+
+    public override void OnShot(string tag)
+    {
+        if (tag == "Eraser")
+            Destroy(this.gameObject);
+        else
+            return;
+    }
+}
