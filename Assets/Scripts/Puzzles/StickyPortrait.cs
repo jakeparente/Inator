c9fc96cb0ae5d6dbd3d943d6b0ba5e06711a10db
Jake Parente <jakeparente@gmail.com>
2020-05-07 19:09:11 -0600

    picture frames are scannable

---
+++ Assets/Scripts/Puzzles/StickyPortrait.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPortrait : MaterialGiver
+{
+    public override void OnScan()
+    {
+        inator.LoadInator(backupMaterial.name.Split(char.Parse(" "))[0], backupMaterial);
+    }
+}
